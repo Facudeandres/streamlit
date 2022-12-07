c153fef7d40dd5a3f89cf6a5a16816c483f22a60
Facudeandres <115202933+Facudeandres@users.noreply.github.com>
2022-12-07 10:37:29 -0300

    Add files via upload

---
+++ streamlit_prueba1.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Mi menú de opciones')
+
+if st.button('Punto de Equilibrio'):
+  # aquí iría el código que se ejecutaría al hacer clic en este botón
+  st.write('Has seleccionado el botón "Punto de Equilibrio".')
+
+if st.button('Margen'):
+  # aquí iría el código que se ejecutaría al hacer clic en este botón
+  st.write('Has seleccionado el botón "Margen".')
+
+if st.button('Punto de cierre'):
+  # aquí iría el código que se ejecutaría al hacer clic en este botón
+  st.write('Has seleccionado el botón "Punto de cierre".')
